0c8403b5ad8f422c15c10402469c096094235cf4
Kevin Zhang <kevinzhanglulu@gmail.com>
2020-03-27 18:47:42 +0800

    feat: backup log files

--- electron/log.ts
+++ electron/log.ts
@@ -3,12 +3,18 @@ import * as path from "path";
 import { app } from "electron";
 import { isDev } from "./utils";
 
+const MAX_LOG_FILE_SIZE_MB = 1;
+const LOG_BACKUPS = 3;
+
 log4js.configure({
   appenders: {
     out: { type: "stdout" },
     app: {
       type: "file",
       filename: path.join(app.getPath("userData"), "app.log"),
+      maxLogSize: MAX_LOG_FILE_SIZE_MB * 1024 * 1024,
+      backups: LOG_BACKUPS,
+      compress: true,
     },
   },
   categories: {
