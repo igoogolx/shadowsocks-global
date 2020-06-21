6780e43fb22340045da957eb548a6d8a4c93de03
Kevin Zhang <kevinzhanglulu@gmail.com>
2020-06-21 21:54:33 +0800

    perf: reduce time to load by introducing React Suspense

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,11 @@
 import React, { Suspense } from "react";
 import styles from "./home.module.css";
 import Header from "../Header/Header";
-import Proxies from "../Proxies/Proxies";
-import Footer from "../Footer/Footer";
 import { useSelector } from "react-redux";
 import { AppState } from "../../reducers/rootReducer";
 
+const Proxies = React.lazy(() => import("../Proxies/Proxies"));
+const Footer = React.lazy(() => import("../Footer/Footer"));
 export const Home = React.memo(() => {
   const isConnectingOrConnected = useSelector<AppState, boolean>(
     (state) => state.proxy.isConnected || state.proxy.isProcessing
@@ -16,7 +16,9 @@ export const Home = React.memo(() => {
       <div className={styles.container}>
         <div className={styles.body}>
           <div className={styles.content}>
-            <Proxies />
+            <Suspense fallback={<></>}>
+              <Proxies />
+            </Suspense>
           </div>
         </div>
       </div>
